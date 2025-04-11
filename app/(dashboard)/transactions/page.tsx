7d355c6101eaa3dba2612629a3c82cd5209083dc
lukasbandziukas3 <lukasband02@gmail.com>
2025-04-11 17:06:53 +0500

    working on csv files

--- app/(dashboard)/transactions/page.tsx
+++ app/(dashboard)/transactions/page.tsx
@@ -17,6 +17,9 @@ import { useBulkDeleteTransactions } from "@/features/transactions/api/use-bulk-
 import { useState } from "react";
 import { UploadButton } from "./upload-button";
 import { ImportCard } from "./import-card";
+import { useSelectAccount } from "@/features/accounts/hooks/use-select-account";
+import { toast } from "sonner";
+import { useBulkCreateTransactions } from "@/features/transactions/api/use-bulk-create-transactions";
 
 enum VARIANTS {
   LIST = "LIST",
@@ -31,6 +34,7 @@ const INITIAL_IMPORT_RESULTS = {
 
  
 const TransactionsPage = () => {
+    const [AccountDialog, confirm] = useSelectAccount();
     const [variant, setVariant] = useState<VARIANTS>(VARIANTS.LIST);
     const [importResults, setImportResults] = useState(INITIAL_IMPORT_RESULTS);
 
@@ -46,6 +50,7 @@ const TransactionsPage = () => {
     }
 
     const newTransaction = useNewTransaction();
+    const createTransactions = useBulkCreateTransactions();
     const transactionsQuery = useGetTransactions();
     const deleteTransactions = useBulkDeleteTransactions();
     const transactions = transactionsQuery.data || [];
@@ -56,7 +61,24 @@ const TransactionsPage = () => {
 
     const onSubmitImport = async (
       values: typeof transactionSchema.$inferInsert[],
-    ) => {};
+    ) => {
+      const accountId = await confirm();
+
+      if (!accountId) {
+        return toast.error("Please select an account to continue.");
+      }
+
+      const data = values.map((value) => ({
+        ...value,
+        accountId: accountId as string,
+      }));
+
+      createTransactions.mutate(data, {
+        onSuccess: () => {
+          onCancelImport();
+        },
+      });
+    };
 
     if (transactionsQuery.isLoading) {
       return (
@@ -78,6 +100,7 @@ const TransactionsPage = () => {
     if (variant === VARIANTS.IMPORT) {
       return (
         <>
+          <AccountDialog />
           <ImportCard
             data={importResults.data}
             onCancel={onCancelImport}
