e3eeb9b193ddac0bac5324c13e68d5578a6f31dd
lukasbandziukas3 <lukasband02@gmail.com>
2025-04-19 13:58:22 +0500

    seed categories added

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -31,12 +31,70 @@ const SEED_TRANSACTIONS: typeof transactions.$inferSelect[] = [];
 import { eachDayOfInterval, format } from "date-fns";
 import { convertAmountToMiliunits } from "@/lib/utils";
 
-const generateRandomAmount = (category: typeof categories.$inferInsert)
-=> {
+const generateRandomAmount = (category: typeof categories.$inferInsert) => {
     switch (category.name) {
         case "Rent":
             return Math.random() * 400 + 90;
         case "Utilities":
             return Math.random() * 200 + 50;
+        case "Food":
+            return Math.random() * 30 + 10;
+        case "Transportation":
+        case "Health":
+            return Math.random() * 50 + 15;
+        case "Entertaiment":    
+        case "Clothing":
+        case "Miscellaneous":
+            return Math.random() * 100 + 20;
+        default:
+            return Math.random() * 50 + 10;
+    }
+};
+
+const generateTransactionsForDay = (day: Date) => {
+    const numTransactions = Math.floor(Math.random() * 4) + 1;
+
+    for (let i = 0; i< numTransactions; i++) {
+        const category = SEED_CATEGORIES[Math.floor(Math.random() * 
+        SEED_CATEGORIES.length)];
+        const isExpense = Math.random() > 0.6;
+
+        const amount = generateRandomAmount(category);
+        const formattedAmount = convertAmountToMiliunits(isExpense ? -amount : amount);
+
+        SEED_TRANSACTIONS.push({
+            id: `transactions_${format(day, "yyyy-MM-dd")}_${i}`,
+            accountId: SEED_ACCOUNTS[0].id,
+
+            categoryId: category.id,
+            date: day,
+            amount: formattedAmount,
+            payee: "Merchant",
+            notes: "Random transaction",
+        });
     }
 }
+
+const generateTransactions = () => {
+    const days = eachDayOfInterval({ start: defaultFrom, end: defaultTo});
+    days.forEach(day => generateTransactionsForDay(day));
+};
+
+generateTransactions();
+
+const main = async () => {
+    try {
+        await db.delete(transactions).execute();
+        await db.delete(accounts).execute();
+        await db.delete(categories).execute();
+
+        await db.insert(categories).values(SEED_CATEGORIES).execute();
+        await db.insert(accounts).values(SEED_ACCOUNTS).execute();
+        await db.insert(transactions).values(SEED_TRANSACTIONS).execute();
+    } catch (error) {
+        console.error("Error during seed", error);
+        process.exit(1);
+    }
+};
+
+main();
